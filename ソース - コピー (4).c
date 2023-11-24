70b978c2ace1024a40c1f781808f3c2c472b7240
sirasusann <148539936+sirasusann@users.noreply.github.com>
2023-11-24 19:22:52 +0900

    Add files via upload

---
+++ ソース - コピー (4).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a;
+
+    printf("‘å•¶Žš‚ð‚¢‚ê‚Ä‚­‚¾‚³‚¢F@")G
+        scanf("%c", &a);
+
+    if (a >= 'A' && a <= 'Z')
+    {
+        printf("'%c' ‚ª‘å•¶Žš‚Å‚·", a);
+    }
+} return 0;
+
+
